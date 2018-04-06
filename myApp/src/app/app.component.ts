5cfaa22cb37a1205098df6b5c81436e811089b5d
José Glauber <jose.oliveira@ccc.ufcg.edu.br>
2018-04-05 21:58:57 -0300

    finalizando seção 12 - acessando slider apenas uma vez

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { IntroPage } from '../pages/intro/intro';
-import {ConfigProvider } from '../providers/config/config';
+import { ConfigProvider } from '../providers/config/config';
 
 @Component({
   templateUrl: 'app.html',
@@ -13,6 +13,9 @@ import {ConfigProvider } from '../providers/config/config';
     ConfigProvider
   ]
 })
+
+//localstorage.clear();
+//localstorage.getItem("config");
 export class MyApp {
   rootPage:any = IntroPage;
 
@@ -26,7 +29,13 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
 
-      let config = configProvider.getConfigData;
+      let config = configProvider.getConfigData();
+      if (config == null) {
+        this.rootPage = IntroPage;
+        configProvider.setConfigData(false);
+      } else {
+        this.rootPage = TabsPage;
+      }
       console.log(config);
       statusBar.styleDefault();
       splashScreen.hide();
--- myApp/src/providers/config/config.ts
+++ myApp/src/providers/config/config.ts
@@ -7,19 +7,18 @@ let config_key_name = "config";
 @Injectable()
 export class ConfigProvider {
   
-  private 
+  private config = {
+    showSlide: false,
+    name: "",
+    username: ""
+  }
+
   constructor() {
-    
   }
 
   //recuperar dados do localStorage
   getConfigData(): any {
     return localStorage.getItem(config_key_name);
-  }
-config = {
-    showSlide: false,
-    name: "",
-    username: ""
   }
   //Gravar/modificar dados do localStorage
   //? significa opcional: mandar ou não
