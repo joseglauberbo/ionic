468ed39043262ad54536eca9a19ffff4519b92f9
José Glauber <jose.oliveira@ccc.ufcg.edu.br>
2018-04-05 21:38:48 -0300

    criando estrutura de armazenamento

---
+++ myApp/src/providers/config/config.ts
@@ -0,0 +1,47 @@
+
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+let config_key_name = "config";
+
+@Injectable()
+export class ConfigProvider {
+  
+  private 
+  constructor() {
+    
+  }
+
+  //recuperar dados do localStorage
+  getConfigData(): any {
+    return localStorage.getItem(config_key_name);
+  }
+config = {
+    showSlide: false,
+    name: "",
+    username: ""
+  }
+  //Gravar/modificar dados do localStorage
+  //? significa opcional: mandar ou não
+  setConfigData(showSlide?: boolean, name?: string, username?: string) {
+    let config = {
+      showSlide: false,
+      name: "",
+      username: ""
+    }
+
+    if(showSlide) {
+      config.showSlide = showSlide;
+    }
+
+    if(name) {
+      config.name = name;
+    }
+
+    if(username) {
+      config.username = username;
+    }
+
+    localStorage.setItem(config_key_name, JSON.stringify(config));
+  }
+}
